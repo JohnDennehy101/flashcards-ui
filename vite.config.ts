dfd44c919c709e5250cfe0039c6a35f9f60c0ba6
John Dennehy <johndennehy101@gmail.com>
2025-12-30 19:16:24 +0000

    Add initial header for app

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import svgr from "vite-plugin-svgr"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: [{ find: "@", replacement: "./src" }],
-  },
+  plugins: [react(), tailwindcss(), svgr()],
   test: {
     globals: true,
     environment: "jsdom",
